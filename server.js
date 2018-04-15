7bad4a9aa6541563fffbbc2a65a34d3a9f3aadb4
Syed Afzal <syed.afzal@codenvoi.com>
2018-04-15 18:06:51 +0500

    initial-commit

---
+++ server.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Syed Afzal
+ */
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+
+//this line register your views partials
+hbs.registerPartials(__dirname+ '/views/partials');
+
+//this line define what view engine you will use
+app.set('view engine', 'hbs');
+
+//this line is to make static directory,but if you put that file name index.htnl it will allways render on this path "/"
+app.use(express.static(__dirname + '/public'));
+
+//this line is used to register helper in a project
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('getTextUpperCase', (txt)=>{
+    return txt.toUpperCase();
+})
+
+
+app.get('/', (req,res)=>{
+    res.render('home.hbs',{
+        pageTitle: 'Home',
+        name: "Syed Afzal",
+    })
+})
+
+app.get('/about', (req,res)=>{
+    res.render('home.hbs',{
+        pageTitle: 'About Us',
+        name: "Syed Afzal",
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Server is up on port 3000")
+})
